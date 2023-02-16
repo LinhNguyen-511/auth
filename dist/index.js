54bbeb28e7a3bb9ba365635e4c60765f66952832
Linh Nguyen <mlinhng@gmail.com>
2023-02-16 07:47:23 +0100

    Simple server

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+// import express, dotenv, jwt
+const express = require('express');
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
+// create express app
+const app = express();
+dotenv.config();
+// listen to port 
+const port = process.env.PORT;
+app.listen(port, () => { console.log("halo"); });
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+//# sourceMappingURL=index.js.map
