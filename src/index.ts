00d42dcb3afffd60f3c47b9feff8022d5edcafd9
Linh Nguyen <mlinhng@gmail.com>
2023-02-27 20:48:04 +0100

    Connect to mongodb

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,27 @@ const express = require("express");
 const dotenv = require("dotenv");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
+const { MongoClient } = require("mongodb");
 
 dotenv.config();
 
 // EXPRESS
 const app = express();
+// MONGODB CLIENT
+const client = new MongoClient(process.env.MONGO_URI);
+
+const connectToDatebase = async () => {
+  try {
+    await client.connect();
+    console.log("Connected to database");
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await client.close();
+  }
+};
+
+connectToDatebase();
 
 // CORS:
 const corsOptions = {
