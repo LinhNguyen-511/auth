a166ca2e8ec7ab72fb3093a43f8458e99377008b
Linh Nguyen <mlinhng@gmail.com>
2023-03-04 22:09:15 +0100

    Create middleware to verify credentials and roles

---
+++ middlewares/verifySignup.ts
@@ -0,0 +1,40 @@
+import User from "../models/User";
+import { Request, Response, NextFunction } from "express";
+import RoleEnum from "../models/type/RoleEnum";
+
+function verifyCredentials(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): void {
+  const { userName, email } = request.body;
+
+  User.findOne({ $or: [{ userName }, { email }] }).exec((error, user) => {
+    if (error) {
+      response.status(500).send({ message: error });
+    } else if (user) {
+      response
+        .status(400)
+        .send({ message: "Username or email is already in use" });
+    } else {
+      next();
+    }
+  });
+}
+
+function verifyRole(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): void {
+  const { roles } = request.body;
+  const areRolesValid = roles.every((role: string) => RoleEnum[role]);
+
+  if (!areRolesValid) {
+    response.status(400).send({ message: "Invalid role(s)" });
+  } else {
+    next();
+  }
+}
+
+export { verifyCredentials, verifyRole };
